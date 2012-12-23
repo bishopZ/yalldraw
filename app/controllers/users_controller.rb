c8a6676deabd169a2512d349116cd65341987319
trent <megaultraninja@gmail.com>
2012-12-23 13:19:22 -0500

    add userscontroller test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,22 +15,20 @@ class UsersController < ApplicationController
   end
 
   def login
-    unless params.key? :user || params[:user][:name] || params[:user][:password]
-      input_error 'All credentials must be filled out', users_path
-    end
-
     user = User.login params[:user][:name], params[:user][:password]
 
     if user
       session[:user_id] = user.id
       redirect_to user_path user
     else
-      input_error 'User name does not match what was expected', users_path
+      flash[:error] = 'User name does not match what was expected'
+      redirect_to users_path
     end
   end
 
   def logout
     session[:user_id] = nil
+    # TODO: root
     redirect_to '/'
   end
 
@@ -42,12 +40,5 @@ class UsersController < ApplicationController
     else
       render :action => :new
     end
-  end
-
-  private
-
-  def input_error(message, path)
-    flash[:error] = message
-    redirect_to path
   end
 end
--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,26 +1,59 @@
 require 'spec_helper'
 
-describe UsersController, 'create a new user' do
+describe UsersController, 'Registering a new user' do
   let(:user_params){ { :user => { :name => 'trent', :password => 'my password', :password1 => 'my password' } } }
 
-  it 'should redirect to drawing create on succesful create' do
-    User.any_instance.stubs(:valid?).returns(true)
-    post 'create', user_params
-    assigns[:user].should_not be_new_record
-    flash[:notice].should_not be_nil
-    response.should redirect_to(new_drawing_path)
+  context 'when successful registration' do
+    it 'should redirect to drawing create' do
+      User.any_instance.stubs(:valid?).returns(true)
+      post 'create', user_params
+      assigns[:user].should_not be_new_record
+      flash[:notice].should_not be_nil
+      response.should redirect_to(new_drawing_path)
+    end
   end
 
-  it 'should render the new template on failed save' do
-    User.any_instance.stubs(:valid?).returns(false)
-    post 'create', user_params
-    assigns[:user].should be_new_record
-    flash[:notice].should be_nil
-    response.should render_template(:new)
+  context 'when failed registration' do
+    it 'should render the new template' do
+      User.any_instance.stubs(:valid?).returns(false)
+      post 'create', user_params
+      assigns[:user].should be_new_record
+      flash[:notice].should be_nil
+      response.should render_template(:new)
+    end
   end
 
   it 'should pass params to user' do
     post 'create', user_params
     assigns[:user].name.should == 'trent'
+  end
+end
+
+describe UsersController, 'Login user' do
+  context 'when successful login' do
+    let(:login_params) { { :user => {} } }
+    it 'should set user.id in session' do
+      User.stubs(:login).returns(User.create(id: 3, name: 'super'))
+      post 'login', login_params
+      session[:user_id].should_not be_nil
+    end
+  end
+
+  context 'when failed login' do
+    let(:login_params) { { :user => {} } }
+    it 'should flash error and redirect' do
+      User.stubs(:login).returns(false)
+      post 'login', login_params
+      flash[:error].should_not be_nil
+      response.should redirect_to(users_path)
+    end
+  end
+end
+
+describe UsersController, 'Logout' do
+  it 'should clear session and redirect' do
+    post 'logout'
+    session[:user_id].should be_nil
+    response.should redirect_to '/'
   end
 end
