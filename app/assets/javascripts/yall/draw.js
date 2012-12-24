eea5ae99588af2ae2c1854df1f84ec191b54c176
trent <megaultraninja@gmail.com>
2012-12-24 12:23:15 -0500

    organize init.js to remove interaction code

--- app/assets/javascripts/yall/draw.js
+++ app/assets/javascripts/yall/draw.js
@@ -1,6 +1,8 @@
 $(function() {
   var canvas    = $('canvas')
   if (!canvas.length) { return; }
+
+  // prepare canvas size
   var rawCanvas = canvas[0];
   var context = rawCanvas.getContext('2d');
   var w = canvas.parent().width();
@@ -8,44 +10,10 @@ $(function() {
   rawCanvas.style.width = rawCanvas.width  = w;
   rawCanvas.style.height = rawCanvas.height = h;
 
-  var colorOptions = {
-    realtime: true,
-    invertControls: false,
-    controlStyle: 'raised',
-    alpha: false
-  };
-
-  $('input#fg-color').colorpicker(colorOptions);
-  $('input#bg-color').colorpicker(colorOptions);
-
-  $('#brush-size-slider').slider({
-    min: 0,
-    max: 10,
-    step: .1,
-    value: 6,
-    slide: function() {
-      $('#brush-size-text').val($('#brush-size-slider').slider('value'))
-    }
-  });
-
   yall.init(rawCanvas);
   yall.initTool();
 });
   /**
-   * get canvas context
-   * refresh
-   * serializePath
-   * interface slider
-   * interface colorpicker
-   * call paper setup on canvas
-   * set tools onto paper.tool
-   *
-   * ColorSerializer
-   * StyleSerializer
-   * PathSerializer
-   *
-   * init
-   *
    * Perisister
    *    ajax remove
    *    ajax save
--- app/assets/javascripts/yall/tool-interface.js
+++ app/assets/javascripts/yall/tool-interface.js
@@ -4,6 +4,26 @@ var yall = (function(my) {
   my.initTool = function () {
     tool = new paper.ToolHandler(this.style());
 
+    var colorOptions = {
+      realtime: true,
+      invertControls: false,
+      controlStyle: 'raised',
+      alpha: false
+    };
+
+    $('input#fg-color').colorpicker(colorOptions);
+    $('input#bg-color').colorpicker(colorOptions);
+
+    $('#brush-size-slider').slider({
+      min: 0,
+      max: 10,
+      step: .1,
+      value: 6,
+      slide: function() {
+        $('#brush-size-text').val($('#brush-size-slider').slider('value'))
+      }
+    });
+
     $('.btn-group button').on('click', function(e) {
       toolName = $(e.srcElement).text().toLowerCase();
 
